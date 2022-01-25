092efe68630ca81dd977b28ca6949a5709a741b9
AATHI PIRAKASH P <76241590+thikash@users.noreply.github.com>
2022-01-26 00:50:35 +0530

    Update CheckBox.java

--- CheckBox.java
+++ CheckBox.java
@@ -9,14 +9,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class CheckBox {
-
-	public static <WebElement> void main(String[] args) {
-		// TODO Auto-generated method stub
+public static <WebElement> void main(String[] args) {
 WebDriverManager.chromedriver().setup();
 ChromeDriver box=new ChromeDriver();
+		
 box.get("http://leafground.com/pages/checkbox.html");
 box.manage().window().maximize();
 box.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
 box.findElement(By.xpath("//label[text()='Select the languages that you know?']/following::input")).click();
 box.findElement(By.xpath("//label[text()='Confirm Selenium is checked']/following::input"));
 box.findElement(By.xpath("//label[text()='DeSelect only checked']/following::input[2]")).click();
@@ -26,7 +26,6 @@ box.findElement(By.xpath("//label[text()='Select all below checkboxes ']/followi
 box.findElement(By.xpath("//label[text()='Select all below checkboxes ']/following::input[3]")).click();
 box.findElement(By.xpath("//label[text()='Select all below checkboxes ']/following::input[4]")).click();
 box.findElement(By.xpath("//label[text()='Select all below checkboxes ']/following::input[5]")).click();
-	
-	}
+}
 
 }
